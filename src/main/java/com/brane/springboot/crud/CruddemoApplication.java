bf2ba243e4aca9280ea41f19b7a51eb42f2acdcd
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-07-09 14:25:41 +0200

    We creating Spring Boot app, REST API using Hibernate API with CRUD methods.

---
+++ src/main/java/com/brane/springboot/crud/CruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.brane.springboot.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+}
