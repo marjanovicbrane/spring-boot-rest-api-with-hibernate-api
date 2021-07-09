f38bf1994b85bdac7b5356bd3ca1a12d44a2f738
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-07-09 14:50:44 +0200

    We created entity class Employee and mapped this class to table employee from database using ORM.

---
+++ src/main/java/com/brane/springboot/crud/entity/Employee.java
@@ -0,0 +1,93 @@
+package com.brane.springboot.crud.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//mapping this entity class to table employee from database
+@Entity
+@Table(name="employee")
+public class Employee {
+
+	@Id//primary key
+	@GeneratedValue(strategy=GenerationType.IDENTITY)//auto increment
+	@Column(name="id")//mapping column from db
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	
+	//default constructor
+	public Employee() {
+		
+	}
+
+	
+	//we don't add field id in constructor, because it will be autommacly generated from database
+	//because it is auto increment
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+
+	//generate getters and setters
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	//adding to string method for debugging
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ "]";
+	}
+}
+
